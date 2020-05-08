d5755130fbfa8d049e434e9ec604958ba31a1f4c
nitunwa <nitun.2@gmail.com>
2020-05-08 12:19:31 -0700

    add method to find bread and pastry

--- Models/bakery.cs
+++ Models/bakery.cs
@@ -1,14 +1,18 @@
 
 namespace Bakery.Models{
   public class Bread{
+   // public int BreadId{get;set;}
     public string BreadName{get;set;}
-    public string BreadPrice{get;set;}
+    public int BreadPrice{get;set;}
     
     public Bread(string name , int price){
       BreadName = name;
       BreadPrice = price;
+     // BreadId = IdIncrement();
     }
-    
+    // private int IdIncrement(){
+    //   BreadId++;
+    // }
     public int TotalBreadPrice(int Item){
       int Breadprice = 3;
       int Total =  Item *  Breadprice;
@@ -17,8 +21,9 @@ namespace Bakery.Models{
   }
 
   public class Pastry{
+   // public int PastryId{get;set;}
     public string PastryName{get;set;}
-    public string PastryPrice{get;set;}
+    public int PastryPrice{get;set;}
     
     public Pastry(string name , int price){
       PastryName = name;
--- program.cs
+++ program.cs
@@ -10,7 +10,7 @@ namespace Bakery{
      Bread white = new Bread("White",3);
      Bread sourdough = new Bread("Sourdough",3);
      List<Bread> breadList = new List<Bread>(){wholeGrain,white,sourdough};
-     List<Bread> breadCart = new List<Bread>(){0};
+     List<Bread> breadCart = new List<Bread>(0);
 
      Pastry plainDonut = new Pastry("PlainDonut",2);
      Pastry chocolateDonut = new Pastry("ChocolateDonut",2);
@@ -18,7 +18,34 @@ namespace Bakery{
      Pastry strawberryCupcake  = new Pastry("StrawberryCupcake",2);
      Pastry chocolateCupcake  = new Pastry("ChocolateCupcake",2);
      List<Pastry> pastryList = new List<Pastry>(){plainDonut,chocolateDonut,vanillaCupcake,strawberryCupcake,chocolateCupcake};
+     List<Pastry> pastryCart = new List<Pastry>(0);
+     StartShopping(breadList,breadCart,pastryList,pastryCart);
+    }
+    public static void StartShopping(List<Bread> breadList,List<Bread> breadCart,List<Pastry> pastryList,List<Pastry> pastryCart){
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      Console.WriteLine($"Make a selection to browes! \nOption 1 [For Bread]/ 2 [For Pastry]");
+      string userInput = Console.ReadLine();
+      if(userInput == "1"){
+        FindBread(breadList);
+      }else{
+        FindPastry(pastryList);
+      }
+    }
 
+    // BREAD AREA
+    public static void FindBread(List<Bread> breadList){
+      Console.WriteLine("Hear is the list of Bread");
+      foreach(Bread bread in breadList){
+        Console.WriteLine($"{bread.BreadName}    ${bread.BreadPrice}");
+      }
+    }
+    public static void FindPastry(List<Pastry> pastryList){
+      int count=1;
+      Console.WriteLine("Hear is the list of Pastry");
+      foreach(Pastry pastry in pastryList){
+        Console.WriteLine($"{count}. {pastry.PastryName}    ${pastry.PastryPrice}");
+        count++;
+      }
     }
   }
 }
