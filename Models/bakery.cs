96cf4e7fbde5976c6f9b7965f5c13b883b678445
nitunwa <nitun.2@gmail.com>
2020-05-08 10:27:05 -0700

    add propery to model classes

--- Models/bakery.cs
+++ Models/bakery.cs
@@ -1,10 +1,24 @@
 
 namespace Bakery.Models{
   public class Bread{
+    public string BreadName{get;set;}
+    public string BreadPrice{get;set;}
+    
+    public Bread(string name , int price){
+      BreadName = name;
+      BreadPrice = price;
+    }
 
   }
 
   public class Pastry{
+    public string PastryName{get;set;}
+    public string PastryPrice{get;set;}
     
+    public Pastry(string name , int price){
+      PastryName = name;
+      PastryPrice = price;
+    }
+
   }
 }
